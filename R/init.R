322236c0c5a2fc0f5132c84b158e8ea3ccf3cd8c
Ben Domingue <ben.domingue@gmail.com>
2012-08-08 08:44:37 -0600

    pulled dps_init in favor of .onLoad()

--- R/init.R
+++ R/init.R
@@ -1,6 +1,7 @@
-dps_init<-function() {
+.onLoad<-function(libname,pkgname) {
   #this function basically just sets the working directory to the DPS_VAM data directory and other things.
   Sys.getenv("USER")->user
+  message("Switching working directory to data directory...")
   switch(user,
          bd={
            setwd("~/Dropbox/DPS_VAM/project/data")
